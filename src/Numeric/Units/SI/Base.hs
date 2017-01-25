b04e2f8f325b0767bad05707cfd0da164c0d2d5c
Yurii Batrak <joewkr@gmail.com>
2017-01-25 21:42:35 +0100

    Adds ticks to prevent -Wunticked-promoted-constructors warnings.

--- src/Numeric/Units/SI/Base.hs
+++ src/Numeric/Units/SI/Base.hs
@@ -4,30 +4,40 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
-module Numeric.Units.SI.Base(Unit(Kg, M, S, A, K, Mol, Cd, I),
+module Numeric.Units.SI.Base(Unit,
+    Kg, M, S, A, K, Mol, Cd, I,
     Mult, NormalForm, Div, type(*), type(/), type(^)) where
 
 import Numeric.Units.SI.Numerals
 
+type Kg = 'Kg_
+type M = 'M_
+type S = 'S_
+type A = 'A_
+type K = 'K_
+type Mol = 'Mol_
+type Cd = 'Cd_
+type I = 'I_
+
 data Unit where
-    Kg :: Unit -- kilogram
-    M :: Unit -- metre
-    S :: Unit -- second
-    A :: Unit -- ampere
-    K :: Unit -- kelvin
-    Mol :: Unit -- mole
-    Cd :: Unit -- candela
-    I :: Unit -- dimensionless
+    Kg_ :: Unit -- kilogram
+    M_ :: Unit -- metre
+    S_ :: Unit -- second
+    A_ :: Unit -- ampere
+    K_ :: Unit -- kelvin
+    Mol_ :: Unit -- mole
+    Cd_ :: Unit -- candela
+    I_ :: Unit -- dimensionless
     (:*:) :: Unit -> Unit -> Unit
     (:/:) :: Unit -> Unit -> Unit
     (:^:) :: Unit -> Exp -> Unit
 
 type family (*) (a :: Unit) (b :: Unit) :: Unit where
-    (*) a b = NormalForm (a :*: b)
+    (*) a b = NormalForm (a ':*: b)
 type family (/) (a :: Unit) (b :: Unit) :: Unit where
-    (/) a b = NormalForm (a :/: b)
+    (/) a b = NormalForm (a ':/: b)
 type family (^) (a :: Unit) (b :: Exp) :: Unit where
-    (^) a b = NormalForm (a :^: b)
+    (^) a b = NormalForm (a ':^: b)
 
 infixr 8 ^
 infixl 7 *, /
@@ -47,35 +57,35 @@ data Gather where
     GZ :: Gather
 
 type family Concat (a :: Gather) (b :: Gather) :: Gather where
-    Concat GZ b = b
-    Concat a GZ = a
-    Concat (GU u rest) b = GU u (Concat rest b)
-    Concat (GD u rest) b = GD u (Concat rest b)
+    Concat 'GZ b = b
+    Concat a 'GZ = a
+    Concat ('GU u rest) b = 'GU u (Concat rest b)
+    Concat ('GD u rest) b = 'GD u (Concat rest b)
 
 type family CombineUntis (a :: UnitsSpec) (b :: Unit) :: UnitsSpec where
-    CombineUntis (US kg m s a k mol cd) I = US kg m s a k mol cd
+    CombineUntis ('US kg m s a k mol cd) I = 'US kg m s a k mol cd
 
-    CombineUntis (US kg m s a k mol cd) (Kg  :^: exp) = US (kg.+.exp) m s a k mol cd
-    CombineUntis (US kg m s a k mol cd) (M   :^: exp) = US kg (m.+.exp) s a k mol cd
-    CombineUntis (US kg m s a k mol cd) (S   :^: exp) = US kg m (s.+.exp) a k mol cd
-    CombineUntis (US kg m s a k mol cd) (A   :^: exp) = US kg m s (a.+.exp) k mol cd
-    CombineUntis (US kg m s a k mol cd) (K   :^: exp) = US kg m s a (k.+.exp) mol cd
-    CombineUntis (US kg m s a k mol cd) (Mol :^: exp) = US kg m s a k (mol.+.exp) cd
-    CombineUntis (US kg m s a k mol cd) (Cd  :^: exp) = US kg m s a k mol (cd.+.exp)
+    CombineUntis ('US kg m s a k mol cd) (Kg  ':^: exp) = 'US (kg.+.exp) m s a k mol cd
+    CombineUntis ('US kg m s a k mol cd) (M   ':^: exp) = 'US kg (m.+.exp) s a k mol cd
+    CombineUntis ('US kg m s a k mol cd) (S   ':^: exp) = 'US kg m (s.+.exp) a k mol cd
+    CombineUntis ('US kg m s a k mol cd) (A   ':^: exp) = 'US kg m s (a.+.exp) k mol cd
+    CombineUntis ('US kg m s a k mol cd) (K   ':^: exp) = 'US kg m s a (k.+.exp) mol cd
+    CombineUntis ('US kg m s a k mol cd) (Mol ':^: exp) = 'US kg m s a k (mol.+.exp) cd
+    CombineUntis ('US kg m s a k mol cd) (Cd  ':^: exp) = 'US kg m s a k mol (cd.+.exp)
 
 type family Switch (op :: Unit -> Gather -> Gather) :: Unit -> Gather -> Gather where
-    Switch GU = GD
-    Switch GD = GU
+    Switch 'GU = 'GD
+    Switch 'GD = 'GU
 
 type family Split (op :: Unit -> Gather -> Gather) (a :: Unit) :: Gather where
-    Split op (a :*: b) = Concat (Split op a) (Split op b)
-    Split op (a :/: b) = Concat (Split op a) (Split (Switch op) b)
-    Split op a = op a GZ
+    Split op (a ':*: b) = Concat (Split op a) (Split op b)
+    Split op (a ':/: b) = Concat (Split op a) (Split (Switch op) b)
+    Split op a = op a 'GZ
 
 type family Group (a :: DimSpec) (b :: Gather) :: DimSpec where
-    Group (DS up down) (GU u rest) = Group (DS (CombineUntis up u) down) rest
-    Group (DS up down) (GD u rest) = Group (DS up (CombineUntis down u)) rest
-    Group grp GZ = grp
+    Group ('DS up down) ('GU u rest) = Group ('DS (CombineUntis up u) down) rest
+    Group ('DS up down) ('GD u rest) = Group ('DS up (CombineUntis down u)) rest
+    Group grp 'GZ = grp
 
 type family Dif1 (a :: Exp) (b :: Exp) :: Exp where
     Dif1 a b = If (a .>. b ) (a .-. b ) PZ
@@ -84,26 +94,26 @@ type family Dif2 (a :: Exp) (b :: Exp) :: Exp where
     Dif2 a b = If (a .>. b ) PZ (b .-. a)
 
 type family If (c :: Boolean) (a :: Exp) (b :: Exp) :: Exp where
-    If BT a b = a
-    If BF a b = b
+    If 'BT a b = a
+    If 'BF a b = b
 
 type family RemoveDups (a :: DimSpec) :: DimSpec where
-    RemoveDups (DS (US kg1 m1 s1 a1 k1 mol1 cd1) (US kg2 m2 s2 a2 k2 mol2 cd2)) = DS
-        (US (Dif1 kg1 kg2) (Dif1 m1 m2) (Dif1 s1 s2) (Dif1 a1 a2) (Dif1 k1 k2) (Dif1 mol1 mol2) (Dif1 cd1 cd2))
-        (US (Dif2 kg1 kg2) (Dif2 m1 m2) (Dif2 s1 s2) (Dif2 a1 a2) (Dif2 k1 k2) (Dif2 mol1 mol2) (Dif2 cd1 cd2))
+    RemoveDups ('DS ('US kg1 m1 s1 a1 k1 mol1 cd1) ('US kg2 m2 s2 a2 k2 mol2 cd2)) = 'DS
+        ('US (Dif1 kg1 kg2) (Dif1 m1 m2) (Dif1 s1 s2) (Dif1 a1 a2) (Dif1 k1 k2) (Dif1 mol1 mol2) (Dif1 cd1 cd2))
+        ('US (Dif2 kg1 kg2) (Dif2 m1 m2) (Dif2 s1 s2) (Dif2 a1 a2) (Dif2 k1 k2) (Dif2 mol1 mol2) (Dif2 cd1 cd2))
 
 type family Normalize (a :: DimSpec) :: Unit where
-    Normalize (DS (US kg1 m1 s1 a1 k1 mol1 cd1) (US kg2 m2 s2 a2 k2 mol2 cd2)) =
-        (Kg :^: kg1 :*: M :^: m1 :*: S :^: s1 :*: A :^: a1 :*: K :^: k1 :*: Mol :^: mol1 :*: Cd :^: cd1) :/:
-        (Kg :^: kg2 :*: M :^: m2 :*: S :^: s2 :*: A :^: a2 :*: K :^: k2 :*: Mol :^: mol2 :*: Cd :^: cd2)
+    Normalize ('DS ('US kg1 m1 s1 a1 k1 mol1 cd1) ('US kg2 m2 s2 a2 k2 mol2 cd2)) =
+        (Kg ':^: kg1 ':*: M ':^: m1 ':*: S ':^: s1 ':*: A ':^: a1 ':*: K ':^: k1 ':*: Mol ':^: mol1 ':*: Cd ':^: cd1) ':/:
+        (Kg ':^: kg2 ':*: M ':^: m2 ':*: S ':^: s2 ':*: A ':^: a2 ':*: K ':^: k2 ':*: Mol ':^: mol2 ':*: Cd ':^: cd2)
 
 type family Simplify (a :: Unit) :: Unit where
     Simplify I = I
-    Simplify (a :^: PZ) = I
+    Simplify (a ':^: PZ) = I
 
-    Simplify (a :*: b) = CombineMult (Simplify a) (Simplify b)
-    Simplify (a :/: b) = CombineDiv (Simplify a) (Simplify b)
-    Simplify (a :^: exp) = CombineExp (Simplify a) exp
+    Simplify (a ':*: b) = CombineMult (Simplify a) (Simplify b)
+    Simplify (a ':/: b) = CombineDiv (Simplify a) (Simplify b)
+    Simplify (a ':^: exp) = CombineExp (Simplify a) exp
 
     Simplify a = a
 
@@ -111,34 +121,34 @@ type family CombineMult (a :: Unit) (b :: Unit) :: Unit where
     CombineMult I I = I
     CombineMult a I = a
     CombineMult I b = b
-    CombineMult a b = a :*: b
+    CombineMult a b = a ':*: b
 
 type family CombineDiv (a :: Unit) (b :: Unit) :: Unit where
     CombineDiv I I = I
     CombineDiv a I = a
-    CombineDiv a b = a :/: b
+    CombineDiv a b = a ':/: b
 
 type family CombineExp (a :: Unit) (b :: Exp) :: Unit where
     CombineExp I e = I
     CombineExp a P1 = a
-    CombineExp a e = a :^: e
+    CombineExp a e = a ':^: e
 
 type family PutPowers (e :: Exp) (a :: Unit) :: Unit where
     PutPowers e I = I
-    PutPowers e (a :*: b) = PutPowers e a :*: PutPowers e b
-    PutPowers e (a :/: b) = PutPowers e a :/: PutPowers e b
-    PutPowers e (a :^: exp) = a :^: (exp .*. e)
+    PutPowers e (a ':*: b) = PutPowers e a ':*: PutPowers e b
+    PutPowers e (a ':/: b) = PutPowers e a ':/: PutPowers e b
+    PutPowers e (a ':^: exp) = a ':^: (exp .*. e)
 
-    PutPowers e a = a :^: e
+    PutPowers e a = a ':^: e
 
 type family PropagateOuterPower (a :: Unit) :: Unit where
-    PropagateOuterPower (a :^: exp) = PutPowers exp a
+    PropagateOuterPower (a ':^: exp) = PutPowers exp a
     PropagateOuterPower a = PutPowers P1 a
 
-type EmptyUS = US PZ PZ PZ PZ PZ PZ PZ
+type EmptyUS = 'US PZ PZ PZ PZ PZ PZ PZ
 
 type NormalForm (a :: Unit) =
-    Simplify (Normalize (RemoveDups (Group (DS EmptyUS EmptyUS) (Split GU (PropagateOuterPower a)))))
+    Simplify (Normalize (RemoveDups (Group ('DS EmptyUS EmptyUS) (Split 'GU (PropagateOuterPower a)))))
 
 type Mult a b = a * b
 type Div a b = a / b
--- src/Numeric/Units/SI/Numerals.hs
+++ src/Numeric/Units/SI/Numerals.hs
@@ -11,86 +11,86 @@ data Boolean where
     BT :: Boolean
     BF :: Boolean
 
-type PZ = UZ :%: U1
+type PZ = 'UZ ':%: U1
 
-type P1 = U1 :%: U1
-type P2 = U2 :%: U1
-type P3 = U3 :%: U1
-type P4 = U4 :%: U1
-type P5 = U5 :%: U1
-type P6 = U6 :%: U1
-type P7 = U7 :%: U1
-type P8 = U8 :%: U1
-type P9 = U9 :%: U1
+type P1 = U1 ':%: U1
+type P2 = U2 ':%: U1
+type P3 = U3 ':%: U1
+type P4 = U4 ':%: U1
+type P5 = U5 ':%: U1
+type P6 = U6 ':%: U1
+type P7 = U7 ':%: U1
+type P8 = U8 ':%: U1
+type P9 = U9 ':%: U1
 
 data Exp where
     (:%:) :: Unary -> Unary -> Exp
 
 type family (%) (x :: Exp) (y :: Exp) :: Exp where
-    (%) x (y1 :%: y2) = x .*. (y2 :%: y1)
+    (%) x (y1 ':%: y2) = x .*. (y2 ':%: y1)
 
 
 type family (.+.) (x :: Exp) (y :: Exp) :: Exp where
-    (.+.) (x1 :%: x2) (y1 :%: y2) = ComputeIrreducible (((x1 ~*~ y2) ~+~ (x2 ~*~ y1)) :%: (x2 ~*~ y2))
+    (.+.) (x1 ':%: x2) (y1 ':%: y2) = ComputeIrreducible (((x1 ~*~ y2) ~+~ (x2 ~*~ y1)) ':%: (x2 ~*~ y2))
 
 type family (.-.) (x :: Exp) (y :: Exp) :: Exp where
-    (.-.) (x1 :%: x2) (y1 :%: y2) = CheckZero (ComputeIrreducible (((x1 ~*~ y2) ~-~ (x2 ~*~ y1)) :%: (x2 ~*~ y2)))
+    (.-.) (x1 ':%: x2) (y1 ':%: y2) = CheckZero (ComputeIrreducible (((x1 ~*~ y2) ~-~ (x2 ~*~ y1)) ':%: (x2 ~*~ y2)))
 
 type family (.*.) (x :: Exp) (y :: Exp) :: Exp where
-    (.*.) (x1 :%: x2) (y1 :%: y2) = ComputeIrreducible ((x1 ~*~ y1) :%: (x2 ~*~ y2))
+    (.*.) (x1 ':%: x2) (y1 ':%: y2) = ComputeIrreducible ((x1 ~*~ y1) ':%: (x2 ~*~ y2))
 
 type family (.>.) (x :: Exp) (y :: Exp) :: Boolean where
-    (.>.) (x1 :%: x2) (y1 :%: y2) = (x1 ~*~ y2) `Greather` (x2 ~*~ y1)
+    (.>.) (x1 ':%: x2) (y1 ':%: y2) = (x1 ~*~ y2) `Greather` (x2 ~*~ y1)
 
 type family CheckZero (a :: Exp) :: Exp where
-    CheckZero (UZ :%: n) = PZ
+    CheckZero ('UZ ':%: n) = PZ
     CheckZero x = x
 
 type family ComputeIrreducible (a :: Exp) :: Exp where
-    ComputeIrreducible (x :%: y) = Reduce x y :%: Reduce y x
+    ComputeIrreducible (x ':%: y) = Reduce x y ':%: Reduce y x
 
 type Reduce (x :: Unary) (y :: Unary) = FromTernary (FI (ToTernary x) (ToTernary y))
 
 type family Greather (a :: Unary) (b :: Unary) :: Boolean where
-    Greather x UZ = BT
-    Greather UZ y = BF
-    Greather (US x) (US y) = x `Greather` y
+    Greather x 'UZ = 'BT
+    Greather 'UZ y = 'BF
+    Greather ('US x) ('US y) = x `Greather` y
 
 data Unary where
     UZ :: Unary
     US :: Unary -> Unary
 
-type U1 = US UZ
-type U2 = US U1
-type U3 = US U2
-type U4 = US U3
-type U5 = US U4
-type U6 = US U5
-type U7 = US U6
-type U8 = US U7
-type U9 = US U8
+type U1 = 'US 'UZ
+type U2 = 'US  U1
+type U3 = 'US  U2
+type U4 = 'US  U3
+type U5 = 'US  U4
+type U6 = 'US  U5
+type U7 = 'US  U6
+type U8 = 'US  U7
+type U9 = 'US  U8
 
 type family (~+~) (x :: Unary) (y :: Unary) :: Unary where
-    (~+~) x UZ = x
-    (~+~) x (US y) = US (x ~+~ y)
+    (~+~) x 'UZ = x
+    (~+~) x ('US y) = 'US (x ~+~ y)
 
 type family (~-~) (x :: Unary) (y :: Unary) :: Unary where
-    (~-~) x UZ = x
-    (~-~) (US x) (US y) = x ~-~ y
+    (~-~) x 'UZ = x
+    (~-~) ('US x) ('US y) = x ~-~ y
 
 type family (~*~) (x :: Unary) (y :: Unary) :: Unary where
-    (~*~) x UZ = UZ
-    (~*~) x (US y) = x ~+~ (x ~*~ y)
+    (~*~) x 'UZ = 'UZ
+    (~*~) x ('US y) = x ~+~ (x ~*~ y)
 
 type family ToTernary (a :: Unary) :: Ternary where
-    ToTernary UZ = TBot
-    ToTernary (US rest) = T1 TBot + ToTernary rest
+    ToTernary 'UZ = 'TBot
+    ToTernary ('US rest) = 'T1 'TBot + ToTernary rest
 
 type family FromTernary (a :: Ternary) :: Unary where
-    FromTernary TBot = UZ
-    FromTernary (TZ rest) = U3 ~*~ FromTernary rest
-    FromTernary (T1 rest) = (U3 ~*~ FromTernary rest) ~+~ U1
-    FromTernary (TJ rest) = (U3 ~*~ FromTernary rest) ~-~ U1
+    FromTernary 'TBot = 'UZ
+    FromTernary ('TZ rest) = U3 ~*~ FromTernary rest
+    FromTernary ('T1 rest) = (U3 ~*~ FromTernary rest) ~+~ U1
+    FromTernary ('TJ rest) = (U3 ~*~ FromTernary rest) ~-~ U1
 
 data Ternary where
     TBot :: Ternary
@@ -99,74 +99,74 @@ data Ternary where
     TJ :: Ternary -> Ternary
 
 type family Opp (a :: Ternary) :: Ternary where
-    Opp TBot = TBot
-    Opp (TZ rest) = TZ (Opp rest)
-    Opp (T1 rest) = TJ (Opp rest)
-    Opp (TJ rest) = T1 (Opp rest)
+    Opp 'TBot = 'TBot
+    Opp ('TZ rest) = 'TZ (Opp rest)
+    Opp ('T1 rest) = 'TJ (Opp rest)
+    Opp ('TJ rest) = 'T1 (Opp rest)
 
 type family (+) (x :: Ternary) (y :: Ternary) :: Ternary where
-    (+) x TBot = x
-    (+) TBot y = y
+    (+) x 'TBot = x
+    (+) 'TBot y = y
 
-    (+) (TZ x) (TZ y) = TZ (x + y)
-    (+) (TZ x) (T1 y) = T1 (x + y)
-    (+) (TZ x) (TJ y) = TJ (x + y)
+    (+) ('TZ x) ('TZ y) = 'TZ (x + y)
+    (+) ('TZ x) ('T1 y) = 'T1 (x + y)
+    (+) ('TZ x) ('TJ y) = 'TJ (x + y)
 
-    (+) (T1 x) (TZ y) = T1 (x + y)
-    (+) (T1 x) (T1 y) = TJ (x + y + T1 TBot)
-    (+) (T1 x) (TJ y) = TZ (x + y)
+    (+) ('T1 x) ('TZ y) = 'T1 (x + y)
+    (+) ('T1 x) ('T1 y) = 'TJ (x + y + 'T1 'TBot)
+    (+) ('T1 x) ('TJ y) = 'TZ (x + y)
 
-    (+) (TJ x) (TZ y) = TJ (x + y)
-    (+) (TJ x) (T1 y) = TZ (x + y)
-    (+) (TJ x) (TJ y) = T1 (x + y + TJ TBot)
+    (+) ('TJ x) ('TZ y) = 'TJ (x + y)
+    (+) ('TJ x) ('T1 y) = 'TZ (x + y)
+    (+) ('TJ x) ('TJ y) = 'T1 (x + y + 'TJ 'TBot)
 
 type family (-) (x :: Ternary) (y :: Ternary) :: Ternary where
     (-) x y = x + Opp y
 
-type Sign (x :: Ternary) = SignQ x TBot
+type Sign (x :: Ternary) = SignQ x 'TBot
 
 type family SignQ (x :: Ternary) (y :: Ternary) :: Ternary where
-    SignQ TBot y = y
-    SignQ (TZ x) y = SignQ x y
-    SignQ (T1 x) y = SignQ x (T1 TBot)
-    SignQ (TJ x) y = SignQ x (TJ TBot)
+    SignQ 'TBot y = y
+    SignQ ('TZ x) y = SignQ x y
+    SignQ ('T1 x) y = SignQ x ('T1 'TBot)
+    SignQ ('TJ x) y = SignQ x ('TJ 'TBot)
 
-type Abs (x :: Ternary) = AbsQ x x TBot
+type Abs (x :: Ternary) = AbsQ x x 'TBot
 
 type family AbsQ (x :: Ternary) (y :: Ternary) (z ::Ternary) :: Ternary where
-    AbsQ TBot y TBot = TBot
-    AbsQ TBot y (T1 TBot) = y
-    AbsQ TBot y (TJ TBot) = Opp y
+    AbsQ 'TBot y 'TBot = 'TBot
+    AbsQ 'TBot y ('T1 'TBot) = y
+    AbsQ 'TBot y ('TJ 'TBot) = Opp y
 
-    AbsQ (TZ x) y z = AbsQ x y z
-    AbsQ (T1 x) y z = AbsQ x y (T1 TBot)
-    AbsQ (TJ x) y z = AbsQ x y (TJ TBot)
+    AbsQ ('TZ x) y z = AbsQ x y z
+    AbsQ ('T1 x) y z = AbsQ x y ('T1 'TBot)
+    AbsQ ('TJ x) y z = AbsQ x y ('TJ 'TBot)
 
-type Min (x :: Ternary) (y :: Ternary) = MinQ (Abs y - Abs x) x y TBot
-type Min' (x :: Ternary) (y :: Ternary) = MinQ (Abs (T1 y) - Abs (T1 x)) x y TBot
-type Min'' (x :: Ternary) (y :: Ternary) = MinQ (Abs (TJ y) - Abs (TJ x)) x y TBot
+type Min (x :: Ternary) (y :: Ternary) = MinQ (Abs y - Abs x) x y 'TBot
+type Min' (x :: Ternary) (y :: Ternary) = MinQ (Abs ('T1 y) - Abs ('T1 x)) x y 'TBot
+type Min'' (x :: Ternary) (y :: Ternary) = MinQ (Abs ('TJ y) - Abs ('TJ x)) x y 'TBot
 
 type family MinQ (x :: Ternary) (y :: Ternary) (z :: Ternary) (u :: Ternary) :: Ternary where
-    MinQ TBot y z TBot = y
-    MinQ TBot y z (T1 TBot) = y
-    MinQ TBot y z (TJ TBot) = z
+    MinQ 'TBot y z 'TBot = y
+    MinQ 'TBot y z ('T1 'TBot) = y
+    MinQ 'TBot y z ('TJ 'TBot) = z
 
-    MinQ (TZ x) y z u = MinQ x y z u
-    MinQ (T1 x) y z u = MinQ x y z (T1 TBot)
-    MinQ (TJ x) y z u = MinQ x y z (TJ TBot)
+    MinQ ('TZ x) y z u = MinQ x y z u
+    MinQ ('T1 x) y z u = MinQ x y z ('T1 'TBot)
+    MinQ ('TJ x) y z u = MinQ x y z ('TJ 'TBot)
 
 type family FI (x :: Ternary) (y :: Ternary) :: Ternary where
-    FI TBot y = TBot
-    FI x TBot = Sign x
+    FI 'TBot y = 'TBot
+    FI x 'TBot = Sign x
 
-    FI (TZ x) (TZ y) = FI x y
-    FI (TZ x) (T1 y) = TZ (FI x (T1 y))
-    FI (TZ x) (TJ y) = TZ (FI x (TJ y))
+    FI ('TZ x) ('TZ y) = FI x y
+    FI ('TZ x) ('T1 y) = 'TZ (FI x ('T1 y))
+    FI ('TZ x) ('TJ y) = 'TZ (FI x ('TJ y))
 
-    FI (T1 x) (TZ y) = FI (T1 x) y
-    FI (T1 x) (T1 y) = TZ (FI (x - Min' x y) (T1 y)) + FI (Min (T1 x) (T1 y)) (x - y)
-    FI (T1 x) (TJ y) = TZ (FI (x + Min'' (Opp x) y) (TJ y)) + FI (Min (T1 x) (T1 (Opp y))) (x + y)
+    FI ('T1 x) ('TZ y) = FI ('T1 x) y
+    FI ('T1 x) ('T1 y) = 'TZ (FI (x - Min' x y) ('T1 y)) + FI (Min ('T1 x) ('T1 y)) (x - y)
+    FI ('T1 x) ('TJ y) = 'TZ (FI (x + Min'' (Opp x) y) ('TJ y)) + FI (Min ('T1 x) ('T1 (Opp y))) (x + y)
 
-    FI (TJ x) (TZ y) = FI (TJ x) y
-    FI (TJ x) (T1 y) = TZ (FI (x + Min' (Opp x) y) (T1 y)) + FI (Min (TJ x) (TJ (Opp y))) (x + y)
-    FI (TJ x) (TJ y) = TZ (FI (x - Min'' x y) (TJ y)) + FI (Min (TJ x) (TJ y)) (x - y)
+    FI ('TJ x) ('TZ y) = FI ('TJ x) y
+    FI ('TJ x) ('T1 y) = 'TZ (FI (x + Min' (Opp x) y) ('T1 y)) + FI (Min ('TJ x) ('TJ (Opp y))) (x + y)
+    FI ('TJ x) ('TJ y) = 'TZ (FI (x - Min'' x y) ('TJ y)) + FI (Min ('TJ x) ('TJ y)) (x - y)
